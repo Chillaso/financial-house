dd6531425d4d4710bd85c2e95ad2be132c992c95
Chillaso <carlos.chillagonzalez@gmail.com>
2020-11-29 10:58:33 +0100

    Add docker-compose with mongodb

---
+++ _docker/init.js
@@ -0,0 +1,15 @@
+db = new Mongo().getDB("financialHouse");
+db.createCollection('book', {capped: false});
+db.book.insert({
+  "year": 2020,
+  "months": {
+    "month": 11,
+    "entries": {
+      "date": "2020-11-28T00:00:00Z00:00",
+      "type": 0,
+      "amount": 50,
+      "concept": "Gasto fijo",
+      "description": "Gas"
+    }
+  }
+})
