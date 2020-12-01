75dbfe13d41095ceda7316f017c8865cb0c95dd4
Chillaso <carlos.chillagonzalez@gmail.com>
2020-12-02 00:44:56 +0100

    Add all books

--- repository/bookRepository.go
+++ repository/bookRepository.go
@@ -17,20 +17,32 @@ const (
 	MONGOFINANCIALURI = "MONGO_FINANCIAL_URI"
 )
 
-var db *mongo.Database
+var (
+	db *mongo.Database
+)
+
+func GetAllBooks() ([]model.Book, error){
+	books := make([]model.Book, 0, 2)
+	cursor, err := db.Collection(COLLECTION).Find(context.Background(), bson.D{}, options.Find().SetLimit(int64(10)))
+	if cursor != nil && err == nil {
+		ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+		for cursor.Next(ctx) {
+			book := model.Book{}
+			err = cursor.Decode(&book)
+			books = append(books, book)
+		}
+	}
+	return books, err
+}
 
 func GetBookByYearAndMonth(year int, month int) (model.Book, error) {
 
 	query := bson.D{{"year", year}, {"months.month", month}}
 
-	result := db.Collection(COLLECTION).FindOne(context.TODO(), query)
+	result := db.Collection(COLLECTION).FindOne(context.TODO(), &query)
 	book := model.Book{}
 	err := result.Decode(&book)
-	if err != nil {
-		return book, err
-	} else{
-		return book, nil
-	}
+	return book, err
 }
 
 func init() {
--- service/bookService.go
+++ service/bookService.go
@@ -5,6 +5,11 @@ import (
 	"github.com/Chillaso/financial-house/repository"
 )
 
+func GetAllBooks() ([]model.Book, error){
+	return repository.GetAllBooks()
+}
+
 func GetBookByYearAndMonth(year int, month int) (model.Book, error) {
 	return repository.GetBookByYearAndMonth(year, month)
 }
+
